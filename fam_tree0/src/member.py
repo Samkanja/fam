9c4d2564b406eaa008973ac5fcc43dbfc71a348b
Samkanja <samuelkanja4@gmail.com>
2024-02-11 19:45:13 -0800

    added granddy

--- fam_tree0/src/member.py
+++ fam_tree0/src/member.py
@@ -1,12 +1,13 @@
 from enum import Enum, auto, StrEnum
-class Gender(StrEnum):
-    male = 'Male'
-    female  = 'Female'
 
+
+class Gender(StrEnum):
+    male = "Male"
+    female = "Female"
 
 
 class Member:
-    def __init__(self,id, name, gender):
+    def __init__(self, id, name, gender):
         self.id = id
         self.name = name
         self.gender = Gender(gender)
@@ -15,44 +16,44 @@ class Member:
         self.spouse = None
         self.children = []
 
-
     def set_mother(self, mother):
         if not isinstance(mother, Member):
-            raise ValueError('Invalid value for mother')
+            raise ValueError("Invalid value for mother")
         if mother.gender != Gender.female:
-            raise ValueError('ivnalid gender value for mother,\
-                 mother should be female')
+            raise ValueError(
+                "ivnalid gender value for mother,\
+                 mother should be female"
+            )
         self.mother = mother
 
     def set_father(self, father):
         if not isinstance(father, Member):
-            raise ValueError('Invalid value for father')
+            raise ValueError("Invalid value for father")
         if father.gender != Gender.male:
-            raise ValueError('ivnalid gender value for father,\
-                 mother should be male')
+            raise ValueError(
+                "ivnalid gender value for father,\
+                 mother should be male"
+            )
         self.father = father
-
-    
 
     def set_spouse(self, spouse):
         if not isinstance(spouse, Member):
-            raise ValueError('Invalid value for spouse')
+            raise ValueError("Invalid value for spouse")
         if self.gender == spouse.gender:
-            raise ValueError('ivnalid gender value for spouse,\
-                 spouse should be a different Gender.')
+            raise ValueError(
+                "ivnalid gender value for spouse,\
+                 spouse should be a different Gender."
+            )
         self.spouse = spouse
 
     def add_child(self, child):
         if not isinstance(child, Member):
-            raise ValueError('Invalid Value for child')
+            raise ValueError("Invalid Value for child")
         self.children.append(child)
 
-        
-    
-
-
-    
-
-
-
-
+    def get_paternal_grandmother(self):
+        if not self.father:
+            return None
+        if not self.father.mother:
+            return None
+        return self.father.mother
--- fam_tree0/tests/unit/test_member.py
+++ fam_tree0/tests/unit/test_member.py
@@ -10,7 +10,7 @@ class TestMember(TestCase):
         self.assertEqual(isinstance(self.member, Member), True)
 
         self.assertEqual(self.member.id, 1)
-        self.assertEqual(self.member.name, 'Zim')
+        self.assertEqual(self.member.name, "Zim")
         self.assertEqual(self.member.gender, Gender.male)
         self.assertEqual(self.member.mother, None)
         self.assertEqual(self.member.mother, None)
@@ -22,50 +22,50 @@ class TestMember(TestCase):
         self.assertRaises(ValueError, Member, 1, "SomeOtherGuys", "Queer")
 
     def test_set_mother(self):
-        mother_demo1 = 'mother_demo1'
-        mother_demo2 = Member(2, 'motherdenm3', "Male")
+        mother_demo1 = "mother_demo1"
+        mother_demo2 = Member(2, "motherdenm3", "Male")
         mother_demo3 = Member(3, "Mom", "Female")
 
         # Error Cases
         self.assertRaises(ValueError, self.member.set_mother, mother_demo1)
         self.assertRaises(ValueError, self.member.set_mother, mother_demo2)
 
-        # 
+        #
         self.member.set_mother(mother_demo3)
         self.assertEqual(self.member.mother.name, "Mom")
         self.assertEqual(self.member.mother.gender, Gender.female)
-    
+
     def test_set_father(self):
-        father_demo1 = 'father_demo1'
-        father_demo2 = Member(2, 'fatherdenm3', "Female")
+        father_demo1 = "father_demo1"
+        father_demo2 = Member(2, "fatherdenm3", "Female")
         father_demo3 = Member(3, "Dad", "Male")
 
         # Error Cases
         self.assertRaises(ValueError, self.member.set_father, father_demo1)
         self.assertRaises(ValueError, self.member.set_father, father_demo2)
 
-        # 
+        #
         self.member.set_father(father_demo3)
         self.assertEqual(self.member.father.name, "Dad")
         self.assertEqual(self.member.father.gender, Gender.male)
 
     def test_set_spouse(self):
-        spouse_demo1 = 'spouse_demo1'
-        spouse_demo2 = Member(2, 'spousedenm3', "Male")
+        spouse_demo1 = "spouse_demo1"
+        spouse_demo2 = Member(2, "spousedenm3", "Male")
         spouse_demo3 = Member(3, "Spouse", "Female")
 
         # Error Cases
         self.assertRaises(ValueError, self.member.set_spouse, spouse_demo1)
         self.assertRaises(ValueError, self.member.set_spouse, spouse_demo2)
 
-        # 
+        #
         self.member.set_spouse(spouse_demo3)
         self.assertEqual(self.member.spouse.name, "Spouse")
         self.assertEqual(self.member.spouse.gender, Gender.female)
 
     def test_add_child(self):
-        child_demoa = 'child_demoa'
-        child_demob = Member(4, "Daughter","Female")
+        child_demoa = "child_demoa"
+        child_demob = Member(4, "Daughter", "Female")
 
         # error case
         self.assertRaises(ValueError, self.member.add_child, child_demoa)
@@ -73,7 +73,18 @@ class TestMember(TestCase):
         # success rate
         self.member.add_child(child_demob)
         self.assertEqual(len(self.member.children), 1)
-        self.assertEqual(self.member.children[0].name, 'Daughter')
+        self.assertEqual(self.member.children[0].name, "Daughter")
         self.assertEqual(self.member.children[0].gender, Gender.female)
-    
 
+    def test_get_paternal_grandmother(self):
+        member = Member(9, "Newmember", "Male")
+        father = Member(10, "Newmeber2", "Male")
+        grandmother = Member(11, "Newmember_grandmother", "Female")
+
+        self.assertEqual(member.get_paternal_grandmother(), None)
+
+        member.father = father
+        self.assertEqual(member.get_paternal_grandmother(), None)
+
+        member.father.mother = grandmother
+        self.assertEqual(member.get_paternal_grandmother(), grandmother)
